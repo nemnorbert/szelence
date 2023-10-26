8f158eca3d34b1a4471ab99d6fba4c6e112841ad
Norbert Nemeth <nth.norbert@gmail.com>
2023-10-26 17:18:29 +0200

    minor fix

--- index.php
+++ index.php
@@ -1,43 +1,4 @@
-<!DOCTYPE html>
-<html lang="hu">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Szelence, Üvegékszer és ajándékbolt</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-
-<nav>
-    <a href="#home">Főoldal</a>
-    <a href="#galery">Galéria</a>
-    <a href="#about">Rólunk</a>
-    <a href="#contact">Kapcsolat</a>
-</nav>
-<main>
-    <div id="home">
-        <h1>Szelence</h1>
-    </div>
-    <div id="galery">
-        <h2>Galéria</h2>
-    </div>
-    <div id="about">
-        <h2>Rólam</h2>
-    </div>
-    <div id="faq">
-        <h2>Gyakori Kérédsek</h2>
-    </div>
-    <div id="contact">
-        <h2>Kapcsolat</h2>
-    </div>
-</main>
-<footer>
-    <div>Szelence</div>
-    <div>Kapcsolat</div>
-    <div></div>
-    <div></div>
-</footer>
-    
-<script src="js/main.js"></script>
-</body>
-</html>
+<?php
+require_once "php/functions.php";
+require_once "php/main.php";
+?>
---
+++ php/main.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Szelence, Üvegékszer és ajándékbolt</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<header>
+    <div class="logo">Szelence</div>
+    <div class="menu">
+        <a href="#home">Főoldal</a>
+        <a href="#galery">Galéria</a>
+        <a href="#about">Rólunk</a>
+        <a href="#contact">Kapcsolat</a>
+    </div>
+</header>
+<nav>
+    <a href="#home">Főoldal</a>
+    <a href="#galery">Galéria</a>
+    <a href="#about">Rólunk</a>
+    <a href="#contact">Kapcsolat</a>
+</nav>
+<main>
+    <div id="home">
+        <h1>Szelence</h1>
+    </div>
+    <div id="gallery">
+        <h2>Képek</h2>
+    </div>
+    <div id="about">
+        <h2>Rólam</h2>
+    </div>
+    <div id="faq">
+        <h2>Gyakori Kérédsek</h2>
+    </div>
+    <div id="contact">
+        <h2>Kapcsolat</h2>
+        <div>
+            <a href="tel:06305560368">06305560368</a>
+            <a href="mailto:siena@t-online.hu">siena@t-online.hu</a>
+        </div>
+        <div>
+            <a target="_blank" href="https://www.facebook.com/Szelence-%C3%9Cveg%C3%A9kszerbolt-%C3%A9s-M%C5%B1hely-199837833363379/">FB</a>
+            <a target="_blank" href="https://www.instagram.com/szelence/">iG</a>
+        </div>
+    </div>
+</main>
+<footer>
+    <div>Szelence</div>
+    <div>Kapcsolat</div>
+    <div></div>
+    <div>Powered by REDCAT</div>
+</footer>
+    
+<script src="js/main.js"></script>
+</body>
+</html>
